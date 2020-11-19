7126b95322f0a59f06923cb8f9eb7624e8ef532b
Brian Moran <brian@trucentive.com>
2020-11-18 22:42:17 -0800

    alternative params for code retrieval

--- lib/anthracinus/client/realtime_order.rb
+++ lib/anthracinus/client/realtime_order.rb
@@ -6,7 +6,7 @@ module Anthracinus
 
     module RealtimeOrder
       # purchase_order_number up to 50 chars
-      def order_submit_realtime(client_pgm_number, client_ref_id, amount, content_provider_id, purch_order_number, return_card_and_pin = true, payment_type = :draw_down, options={})
+      def order_submit_realtime(client_pgm_number, client_ref_id, amount, content_provider_id, purch_order_number, return_card_and_pin = true, payment_type = :draw_down, options = {})
 
         c_r_id = client_ref_id.to_s
         pmt_type = payment_type.to_s.upcase
@@ -28,21 +28,21 @@ module Anthracinus
             'quantity': '1', # must be one for this API
             'contentProvider': content_provider_id # from the catalog
         }
-        params =  {
-                    'clientProgramNumber': client_pgm_number.to_s,
-                    'paymentType': pmt_type,
-                    'poNumber': purch_order_number.to_s,
-                    'returnCardNumberAndPIN': return_card_and_pin.to_s,
-                    'orderDetails': [order_detail]
-                }
+        params = {
+            'clientProgramNumber': client_pgm_number.to_s,
+            'paymentType': pmt_type,
+            'poNumber': purch_order_number.to_s,
+            'returnCardNumberAndPIN': return_card_and_pin.to_s,
+            'orderDetails': [order_detail]
+        }
         puts("Params #{params.inspect}")
         post(Anthracinus::Client::ServiceUrls::ORDER_REALTIME_BULK_URL, params, options)
       end
 
-      def order_submit_realtime_virtual(client_pgm_number, client_ref_id, amount, purch_order_number, return_card_and_pin = true, payment_type = :draw_down, options={})
+      def order_submit_realtime_virtual(client_pgm_number, client_ref_id, amount, purch_order_number, return_card_and_pin = true, payment_type = :draw_down, options = {})
         c_r_id = client_ref_id.to_s
         pmt_type = payment_type.to_s.upcase
-        
+
         unless validate_client_ref(c_r_id)
           raise InvalidParameterError.new('Client Reference ID contains invalid characters')
         end
@@ -56,34 +56,41 @@ module Anthracinus
         end
 
         order_detail = {
-                    'clientRefId': c_r_id, # something unique
-                    'amount': amount.to_s,
-                    'quantity': '1' # must be one for this API
+            'clientRefId': c_r_id, # something unique
+            'amount': amount.to_s,
+            'quantity': '1' # must be one for this API
 
-                }
+        }
         params = {
-                            'clientProgramNumber': client_pgm_number.to_s,
-                            'paymentType': pmt_type,
-                            'poNumber': purch_order_number.to_s,
-                            'orderDetails': [order_detail]
+            'clientProgramNumber': client_pgm_number.to_s,
+            'paymentType': pmt_type,
+            'poNumber': purch_order_number.to_s,
+            'orderDetails': [order_detail]
 
-                        }
+        }
         puts params.inspect
         post(Anthracinus::Client::ServiceUrls::ORDER_REALTIME_VIRTUAL_URL, params, options)
       end
 
-      def get_realtime_order(transaction_id, options={})
-        get(Anthracinus::Client::ServiceUrls::ORDER_REALTIME_BULK_URL+"/#{transaction_id}", options)
+      def get_realtime_order(transaction_id, options = {})
+        get(Anthracinus::Client::ServiceUrls::ORDER_REALTIME_BULK_URL + "/#{transaction_id}", options)
       end
 
-      def get_realtime_virtual_order(transaction_id, options={})
+      def get_realtime_virtual_order(transaction_id, options = {})
         get(Anthracinus::Client::ServiceUrls::ORDER_REALTIME_VIRTUAL_URL + "/#{transaction_id}", options)
       end
 
-      def get_virtual_codes(order_number, options = {})
-        params = {
-            'orderNumber': order_number
-        }
+      def get_virtual_codes(order_number, request_id = nil, client_pgm_number = nil, options = {})
+        if order_number
+          params = {
+              'orderNumber': order_number
+          }
+        else
+          params = {
+              'requestId': request_id,
+              'clientProgramNumber': client_pgm_number
+          }
+        end
         get(Anthracinus::Client::ServiceUrls::ORDER_VIRTUAL_CODES_URL, params, options)
       end
 
@@ -95,7 +102,7 @@ module Anthracinus
       end
 
       # retrieve the order with variable params
-      def order(order_number, request_id = nil, client_pgm_number = nil)
+      def order(order_number, request_id = nil, client_pgm_number = nil, options = {})
         raise InvalidParameterError.new('Order: Order number or request_id AND client_pgm_number must be supplied') unless order_number || (request_id && client_pgm_number)
         if order_number
           params = {
@@ -107,7 +114,7 @@ module Anthracinus
               'clientProgramNumber': client_pgm_number
           }
         end
-        get(Anthracinus::Client::ServiceUrls::ORDER_INFO_URL, params)
+        get(Anthracinus::Client::ServiceUrls::ORDER_INFO_URL, params, options)
       end
 
       def validate_client_ref(clientref)
